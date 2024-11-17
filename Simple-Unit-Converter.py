8d3a6281f291fd632144a0c3ce69b937bffbd327
Mike Manolescu <160978321+mikemanolescu@users.noreply.github.com>
2024-11-17 14:10:15 -0500

    Add Pseudocode simple-unit-converter Outline logic for program functions

---
+++ Simple-Unit-Converter.py
@@ -0,0 +1,65 @@
+#Main Program
+#Def main():
+    #Print conversion menu “What are you converting? 1. Temperature or 2. Distance? Please choose a number corresponding to the conversion type”
+    #Input conversion type from the user (1 for Temperature, 2 for Distance)
+    #While input is invalid:
+        #Print error message “This input is not recognized, please enter 1 for Temperature or 2 for Distance”
+        #Ask user for valid input
+       
+    #If conversion is 1:
+        #Function convert_temperature()
+    #Else if conversion is 2:
+        #Function convert_distance()
+
+    #Print “Do you wish to perform another conversion?”
+    #If yes:
+        #Restart main()
+    #Else:
+        #Exit program
+#Function convert_temperature-
+#Def convert_temperature():
+    #Print temperature conversion choices:
+        #1 for Celsius to Fahrenheit
+        #2 for Fahrenheit to Celsius
+    #Input temperature conversion choice
+    #While input is invalid:
+        #Print error message “This input is not recognized, please enter 1 for Temperature or 2 for Distance”
+        #Ask user for valid input
+
+    #Input value to convert
+    #If input contains units, like 20C:
+        #Print error message “This input is not recognized, please enter a number only without units”
+        #Ask user for valid input
+
+    #If choice is 1:
+        #Convert using formula: (value x 9/5) + 32
+        #Set result as converted value with Fahrenheit unit
+    #Else if choice is 2:
+        #Convert using formula: (value - 32) x 9/5
+        #Set result as converted value with Celsius unit
+
+    #Print result: original value with unit = converted value with unit
+
+#Function convert_distance-
+#Def convert_distance():
+    #Print distance conversion choices:
+        #1 for Miles to Kilometers
+        #2 for Kilometers to Miles
+    #Input distance conversion choice
+    #While input is invalid:
+        #Print error message “This input is not recognized, please enter 1 for Miles to Kilometers or 2 for Kilometers to Miles”
+        #Ask user for valid input
+
+    #Input value to convert
+    #If input contains units, like 20 miles:
+        #Print error message: “This input is not recognized, please enter a number only without units”
+        #Ask user for valid input
+
+    #If choice is 1:
+        #Convert using formula: value x 1.60934
+        #Set result as converted value with kilometers as unit
+    #Else if choice is 2:
+        #Convert using formula: value / 1.60934
+        #Set result as converted value with miles as unit
+
+    #Print result: original value with unit = converted value with unit
